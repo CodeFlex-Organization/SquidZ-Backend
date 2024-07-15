83cfc8f4f16758ba90f73aa3a544fa5c087467a7
georginalastra <rocahuapaya@hotmail.com>
2024-07-14 22:40:17 -0500

    Initial commit

---
+++ src/main/java/com/flabum/squidzbackend/SquidzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flabum.squidzbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquidzBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquidzBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flabum/squidzbackend/SquidzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flabum.squidzbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquidzBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
