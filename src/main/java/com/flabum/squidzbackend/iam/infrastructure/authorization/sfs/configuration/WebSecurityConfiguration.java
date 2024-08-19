5a8da7325edca0283bd6845c9274d53a3b534f8f
orlandoroca <rocahuapaya@hotmail.com>
2024-08-18 22:40:10 -0500

    chore: test

--- src/main/java/com/flabum/squidzbackend/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/flabum/squidzbackend/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -93,7 +93,7 @@ public class WebSecurityConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://192.168.1.6:8081")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
